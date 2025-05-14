e3237447ded383a4a0ba5f781e0cd91d9afa9eac
reddy-sravan12 <101791343+reddy-sravan12@users.noreply.github.com>
2025-05-14 21:18:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function stringChop(str, size) {
-  // your code here
+	let out=[]
+	let string=''
+  for(let i=1;i<=str.length;i++){
+	  
+	  
+	  if(i%size===0){
+		  string+=str[i-1]
+		   console.log(string,' ki')
+		  out.push(string)
+		  //return
+		  string='' 
+	  }else if(i===str.length){
+
+	string+=str[i-1]
+	out.push(string)
+	  }else{ 
+	      	string+=str[i-1]
+	  } 
+   }
+   return out 
 }
 
 // Do not change the code below
